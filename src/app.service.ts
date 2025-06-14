d41f0f5f0429faaffad4165688bdf5e29a3fa7d3
parzival <juanestebanrp123@gmail.com>
2025-06-14 11:13:46 -0500

    services.app

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+// src/products/products.service.ts
+import { Injectable } from '@nestjs/common'
+import productsData from '../data/products.json'
+
+interface Product {
+  id: string
+  name: string
+  price: number
+  description: string
+}
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class ProductsService {
+  // Reutiliza el array del JSON
+  private readonly products: Product[] = productsData
+
+  findAll(page = 1, limit = 10) {
+    const start = (page - 1) * limit
+    const end = start + limit
+    return {
+      items: this.products.slice(start, end),
+      total: this.products.length,
+    }
   }
 }
